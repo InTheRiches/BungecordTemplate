0267a54878ff9e646eeb2f688bb5cecedb66ed45
John Doe <intherichesftk@gmail.com>
2023-03-21 22:03:13 -0400

    Added initial files.

---
+++ src/main/java/net/example/bungecordtemplate/BungecordTemplate.java
@@ -0,0 +1,16 @@
+package net.example.bungecordtemplate;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class BungecordTemplate extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
